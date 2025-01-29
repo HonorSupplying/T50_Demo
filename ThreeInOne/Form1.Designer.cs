73a0b31f6331de8238f283e1789ac29610de14fd
farizl <farizl@handsv.com>
2025-01-29 09:20:46 +0700

    first commit

---
+++ ThreeInOne/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SetupSTEL
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefreshPort = new System.Windows.Forms.Button();
+            this.cmb3in1 = new System.Windows.Forms.ComboBox();
+            this.lbl3in1 = new System.Windows.Forms.Label();
+            this.btnTest3in1MCR = new System.Windows.Forms.Button();
+            this.btnTest3in1PIN = new System.Windows.Forms.Button();
+            this.btnTest3in1ID = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRefreshPort
+            // 
+            this.btnRefreshPort.Location = new System.Drawing.Point(18, 12);
+            this.btnRefreshPort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRefreshPort.Name = "btnRefreshPort";
+            this.btnRefreshPort.Size = new System.Drawing.Size(112, 35);
+            this.btnRefreshPort.TabIndex = 2;
+            this.btnRefreshPort.Text = "&Auto Config";
+            this.btnRefreshPort.UseVisualStyleBackColor = true;
+            this.btnRefreshPort.Click += new System.EventHandler(this.btnRefreshPort_Click);
+            // 
+            // cmb3in1
+            // 
+            this.cmb3in1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb3in1.FormattingEnabled = true;
+            this.cmb3in1.Location = new System.Drawing.Point(458, 112);
+            this.cmb3in1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmb3in1.Name = "cmb3in1";
+            this.cmb3in1.Size = new System.Drawing.Size(97, 28);
+            this.cmb3in1.TabIndex = 7;
+            this.cmb3in1.SelectedIndexChanged += new System.EventHandler(this.cmb3in1_SelectedIndexChanged);
+            // 
+            // lbl3in1
+            // 
+            this.lbl3in1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl3in1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lbl3in1.Location = new System.Drawing.Point(19, 52);
+            this.lbl3in1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl3in1.Name = "lbl3in1";
+            this.lbl3in1.Size = new System.Drawing.Size(830, 149);
+            this.lbl3in1.TabIndex = 6;
+            this.lbl3in1.Text = "Feitian PinPad 3 in 1";
+            this.lbl3in1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl3in1.Click += new System.EventHandler(this.lbl3in1_Click);
+            // 
+            // btnTest3in1MCR
+            // 
+            this.btnTest3in1MCR.Enabled = false;
+            this.btnTest3in1MCR.Location = new System.Drawing.Point(596, 63);
+            this.btnTest3in1MCR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTest3in1MCR.Name = "btnTest3in1MCR";
+            this.btnTest3in1MCR.Size = new System.Drawing.Size(225, 35);
+            this.btnTest3in1MCR.TabIndex = 9;
+            this.btnTest3in1MCR.Text = "MCR";
+            this.btnTest3in1MCR.UseVisualStyleBackColor = true;
+            this.btnTest3in1MCR.Click += new System.EventHandler(this.btnTest3in1MCR_Click);
+            // 
+            // btnTest3in1PIN
+            // 
+            this.btnTest3in1PIN.Enabled = false;
+            this.btnTest3in1PIN.Location = new System.Drawing.Point(596, 108);
+            this.btnTest3in1PIN.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTest3in1PIN.Name = "btnTest3in1PIN";
+            this.btnTest3in1PIN.Size = new System.Drawing.Size(225, 35);
+            this.btnTest3in1PIN.TabIndex = 10;
+            this.btnTest3in1PIN.Text = "PIN Pad";
+            this.btnTest3in1PIN.UseVisualStyleBackColor = true;
+            this.btnTest3in1PIN.Click += new System.EventHandler(this.btnTest3in1PIN_Click);
+            // 
+            // btnTest3in1ID
+            // 
+            this.btnTest3in1ID.Enabled = false;
+            this.btnTest3in1ID.Location = new System.Drawing.Point(596, 153);
+            this.btnTest3in1ID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTest3in1ID.Name = "btnTest3in1ID";
+            this.btnTest3in1ID.Size = new System.Drawing.Size(225, 35);
+            this.btnTest3in1ID.TabIndex = 11;
+            this.btnTest3in1ID.Text = "ID Card";
+            this.btnTest3in1ID.UseVisualStyleBackColor = true;
+            this.btnTest3in1ID.Click += new System.EventHandler(this.btnTest3in1ID_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Location = new System.Drawing.Point(18, 229);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(831, 528);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(14, 27);
+            this.lblStatus.Multiline = true;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.lblStatus.Size = new System.Drawing.Size(804, 477);
+            this.lblStatus.TabIndex = 23;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 929);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnTest3in1ID);
+            this.Controls.Add(this.btnTest3in1PIN);
+            this.Controls.Add(this.btnTest3in1MCR);
+            this.Controls.Add(this.cmb3in1);
+            this.Controls.Add(this.lbl3in1);
+            this.Controls.Add(this.btnRefreshPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(889, 985);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "3in1 Demo Program";
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRefreshPort;
+        private System.Windows.Forms.ComboBox cmb3in1;
+        private System.Windows.Forms.Label lbl3in1;
+        private System.Windows.Forms.Button btnTest3in1MCR;
+        private System.Windows.Forms.Button btnTest3in1PIN;
+        private System.Windows.Forms.Button btnTest3in1ID;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox lblStatus;
+    }
+}
+
---
+++ ThreeInOne/Form1.cs
@@ -0,0 +1,454 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SetupSTEL
+{
+    public partial class frmMain : Form
+    {
+        private static frmMain form = null;
+        private const int WM_SIGNPAD = 0x44A;
+
+        private int _count = 0;
+    
+        public class SmartCardPosResponse
+        {
+            public string idNbr { get; set; }
+            public string thaiNameTitle { get; set; }
+            public string thaiFirstName { get; set; }
+            public string thaiMiddleName { get; set; }
+            public string thaiLastName { get; set; }
+            public string thaiName { get; set; }
+            public string engNameTitle { get; set; }
+            public string engFirstName { get; set; }
+            public string engMiddleName { get; set; }
+            public string engLastName { get; set; }
+            public string engName { get; set; }
+            public string birthDateText { get; set; }
+            public long dateOfBirth { get; set; }
+            public string issueDateText { get; set; }
+            public long issueDate { get; set; }
+            public string expiryDateText { get; set; }
+            public long expiryDate { get; set; }
+            public string addressLine1 { get; set; }
+            public string addressLine2 { get; set; }
+            public string addressLine3 { get; set; }
+            public string addressLine4 { get; set; }
+            public string road { get; set; }
+            public string subDistrict { get; set; }
+            public string district { get; set; }
+            public string province { get; set; }
+            public long errorCode { get; set; }
+            public bool isCardExpired { get; set; }
+            public string laserId { get; set; }
+            public string bp1No { get; set; }
+            public string chipNo { get; set; }
+            public string deviceResponse { get; set; }
+        }
+        private class ConfigPort
+        {
+            public int port { get; set; }
+        }
+        private class CPosDeviceDLL
+        {
+            public bool connected = false;
+            public bool connecting = false;
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern void PosDeviceOpen();
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern void PosDeviceClose();
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosMsreRead(byte[] buff);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+            public static extern int PosMsreWrite(string AccountId);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosSelect();
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosCancel();
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosCheckPort(int com);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+            public static extern int PosPinLoadMK(string KeyBuffer);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosPinReadKey(byte[] outData, int timeOut);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosPinReadKeyClear(byte[] outData, int timeOut);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosThaiIDRead(byte[] buff, int mode);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int PosEMVRead(byte[] buff);
+
+            [DllImport("middleware.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+            public static extern int PosExec(byte[] outdata, byte[] buff, byte[] data);
+
+            private static string ToHexString(byte[] hex, int size)
+            {
+                if (hex == null) return null;
+                var s = new StringBuilder();
+                for (int i = 0; i < size; i++)
+                {
+                    s.Append(hex[i].ToString("X2"));
+                }
+                return s.ToString();
+            }
+
+            public CPosDeviceDLL()
+            {
+                PosDeviceOpen();
+            }
+
+            ~CPosDeviceDLL()
+            {
+                PosDeviceClose();
+            }
+
+            public int Select()
+            {
+                return PosSelect();
+            }
+
+            public int Read(ref string buff)
+            {
+                byte[] trk2 = new byte[300];
+                int ret = PosMsreRead(trk2);
+                buff = System.Text.Encoding.ASCII.GetString(trk2);
+                return ret;
+            }
+
+            public int EMVRead(ref string buff)
+            {
+                byte[] trk2 = new byte[1024];
+                int ret = PosEMVRead(trk2);
+                buff = System.Text.Encoding.ASCII.GetString(trk2);
+                return ret;
+            }
+            public int ThaiIDRead(ref string buff, int mode)
+            {
+                byte[] trk2 = new byte[2048];
+                int ret = PosThaiIDRead(trk2, mode);
+                buff = System.Text.Encoding.UTF8.GetString(trk2);
+                return ret;
+            }
+            public int ThaiIDPhoto(ref byte[] buff)
+            {
+                //byte[] trk2 = new byte[1024];
+                return PosThaiIDRead(buff, 3);
+                //buff = System.Text.Encoding.UTF8.GetString(trk2);
+                //return ret;
+            }
+            public int Cancel()
+            {
+                return PosCancel();
+            }
+            public int CheckPort(int com)
+            {
+                return PosCheckPort(com);
+            }
+
+            public int Load(string buff)
+            {
+                string s = buff + char.MinValue;
+                return PosPinLoadMK(s);
+            }
+
+            public int Read(ref string buff, int timeOut)
+            {
+                byte[] dummy = new byte[300];
+                int ret = PosPinReadKey(dummy, timeOut);
+                buff = ToHexString(dummy, 8);
+                //buff = System.Text.Encoding.ASCII.GetString(dummy);
+                return ret;
+            }
+            public int ReadClear(ref string buff, int timeOut)
+            {
+                byte[] dummy = new byte[300];
+                int ret = PosPinReadKeyClear(dummy, timeOut);
+                buff = ToHexString(dummy, 8);
+                //buff = buff.Replace("F", "");
+                buff = System.Text.Encoding.ASCII.GetString(dummy);
+                return ret;
+            }
+            public int Exec(ref string outdata, string buff, string data)
+            {
+                byte[] dummy = new byte[100000];
+                string s = buff + char.MinValue;
+                Encoding utf8 = new UTF8Encoding(true);
+                byte[] cmd1 = utf8.GetBytes(s);
+                s = data + char.MinValue;
+                byte[] cmd2 = utf8.GetBytes(s);
+                int ret = PosExec(dummy, cmd1, cmd2);
+                buff = ToHexString(dummy, 16);
+                //MessageBox.Show(System.Text.Encoding.ASCII.GetString(dummy));
+                //TextData.Text = dummy;
+                outdata = System.Text.Encoding.UTF8.GetString(dummy);
+                return ret;
+            }
+        }
+
+        private CPosDeviceDLL threeInOne;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            form = this;
+
+        }
+        void loadPorts()
+        {
+            //----- Get Available Port -----//
+            cmb3in1.Items.Clear();
+
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+
+                cmb3in1.Items.Add("(Select)"); cmb3in1.Enabled = true;
+
+                foreach (string port in ports)
+                {
+                
+                    cmb3in1.Items.Add(port);
+                }
+            }
+            else
+            {
+           
+                cmb3in1.Items.Add("No COM"); cmb3in1.Enabled = false; btnTest3in1MCR.Enabled = false;
+                btnTest3in1PIN.Enabled = false;
+                btnTest3in1ID.Enabled = false;
+            }
+        }
+
+        private void btnRefreshPort_Click(object sender, EventArgs e)
+        {
+            updateStatus("Refresh ports ...");
+            loadPorts();
+            cmb3in1.SelectedIndex = 0;
+
+            //----- Auto Config all ports -----//
+            int indexPort = 1;
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+                foreach (string port in ports)
+                {
+                    updateStatus("Try to config port: " + port.ToString());
+                    if (!threeInOne.connected)
+                    {
+                        updateStatus("Try connect 3in1 with port " + port + " : " + indexPort.ToString());
+                        cmb3in1.SelectedIndex = indexPort; Application.DoEvents();
+                        //cmb3in1_SelectedIndexChanged(null, null);
+                        while (threeInOne.connecting) { }
+                        if (threeInOne.connected) { indexPort++; continue; }
+                    }
+                    indexPort++;
+                    if (threeInOne.connected) break;
+                }
+            }
+            updateStatus("Auto Config Finished");
+        }
+
+
+
+        private void cmb3in1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmb3in1.Text.Substring(0, 3) == "COM")
+            {
+                updateStatus("Connecting to 3 in 1 device ...");
+                int nPort = Convert.ToInt32(Regex.Match(cmb3in1.Text, @"\d+").Value);
+                int ret = threeInOne.CheckPort(nPort);
+                if (ret == 0)
+                {
+                    updateStatus("3 in 1 device is connected"); threeInOne.connected = true;
+                    lbl3in1.BackColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    updateStatus("3 in 1 device is not connected"); threeInOne.connected = false;
+                    lbl3in1.BackColor = System.Drawing.Color.Red;
+                }
+                btnTest3in1MCR.Enabled = (ret == 0);
+                btnTest3in1PIN.Enabled = (ret == 0);
+                btnTest3in1ID.Enabled = (ret == 0);
+            }
+            else threeInOne.connected = false;
+        }
+
+        private void btnTest3in1MCR_Click(object sender, EventArgs e)
+        {
+            string trk2 = "";
+
+            updateStatus("Please swipe card ...");
+            int ret = threeInOne.Read(ref trk2);
+
+            if (ret != 1)
+            {
+                updateStatus("Data = " + trk2);
+                return;
+            }
+            else
+            {
+                updateStatus("No Data");
+            }
+        }
+
+        private void btnTest3in1PIN_Click(object sender, EventArgs e)
+        {
+            string trk2 = "";
+            _count++;
+
+            updateStatus("Please press PIN ...");
+            int ret = _count % 2 == 0
+                ? threeInOne.Read(ref trk2, 1000)
+                : threeInOne.ReadClear(ref trk2, 1000);
+
+            if (ret != 1)
+            {
+                updateStatus("Data = " + trk2);
+            }
+            else
+            {
+                updateStatus("No Data");
+            }
+        }
+
+
+        void updateStatus(string s)
+        {
+            lblStatus.Text = s + "\r\n" + lblStatus.Text;
+            lblStatus.Update();
+        }
+
+
+        private void btnTest3in1ID_Click(object sender, EventArgs e)
+        {
+            string smartcard0 = "{ \"format\":\"SCREEN\", \"slide\":\"smartcard-0.json\" }";
+            string smartcard1 = "{ \"format\":\"SCREEN\", \"slide\":\"smartcard-1.json\" }";
+            string checkinsert = "{ \"format\":\"APDU\", \"cmd\":\"card\" }";
+            string slide0 = "{ \"format\":\"SCREEN\", \"slide\":\"slide0.json\" }";
+            string outdata = "";
+            int ret;
+            ret = threeInOne.Exec(ref outdata, smartcard0, "");
+            if (ret != 110)
+            {
+                do
+                {
+                    ret = threeInOne.Exec(ref outdata, checkinsert, "");
+                    if (ret == 0) break;
+                } while (ret != 0);
+
+                if (ret != 0)
+                {
+                    threeInOne.Exec(ref outdata, slide0, "");
+                } else
+                {
+                    threeInOne.Exec(ref outdata, smartcard1, "");
+                    string id = "";
+                    ret = threeInOne.ThaiIDRead(ref id, 0);
+                    try
+                    {
+                        var readResult = JsonConvert.DeserializeObject<SmartCardPosResponse>(id);
+
+                        if (!string.IsNullOrEmpty(readResult.idNbr))
+                        {
+                            updateStatus("idNbr : " + readResult.idNbr.Remove(0, 9).PadLeft(11, 'x'));
+                            updateStatus("ThaiTitle : " + readResult.thaiNameTitle);
+                            updateStatus("ThaiFirstName : " + readResult.thaiFirstName);
+                            updateStatus("ThaiMiddleName : " + readResult.thaiMiddleName);
+                            updateStatus("ThaiLastName : " + readResult.thaiLastName);
+                            updateStatus("ThaiFullName : " + readResult.thaiName);
+                            updateStatus("EngTitle : " + readResult.engNameTitle);
+                            updateStatus("EngFirstName : " + readResult.engFirstName);
+                            updateStatus("EngMiddleName : " + readResult.engMiddleName);
+                            updateStatus("EngLastName : " + readResult.engLastName);
+                            updateStatus("EngFullName : " + readResult.engName);
+                            updateStatus("Birth Date : " + readResult.birthDateText);
+                            updateStatus("Issue Date : " + readResult.issueDateText);
+                            updateStatus("Expire Date : " + readResult.expiryDateText);
+                            updateStatus("AddressLine1 : " + readResult.addressLine1);
+                            updateStatus("AddressLine2 : " + readResult.addressLine2);
+                            updateStatus("AddressLine3 : " + readResult.addressLine3);
+                            updateStatus("AddressLine4 : " + readResult.addressLine4);
+                            updateStatus("Road : " + readResult.road);
+                            updateStatus("Sub District : " + readResult.subDistrict);
+                            updateStatus("Dustrict : " + readResult.district);
+                            updateStatus("Provice : " + readResult.province);
+                            updateStatus("Laser ID : " + readResult.laserId);
+                            updateStatus("BP1 no : " + readResult.bp1No);
+                            updateStatus("Chip no : " + readResult.chipNo);
+
+                        }
+                        else
+                        {
+                            updateStatus("Cannot read IDCard");
+                        }
+                    }
+                    catch
+                    {
+                        updateStatus("Timeout");
+                    }
+                }
+            }
+        }
+
+
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+
+            Application.DoEvents();
+            updateStatus("Load device objects ...");
+            threeInOne = new CPosDeviceDLL();
+
+            updateStatus("Load all ports ..."); Application.DoEvents();
+            loadPorts();
+
+            string config = "";
+
+
+            //----- Load Config 3 in 1 -----//
+            updateStatus("Load 3in1 profile ..."); Application.DoEvents();
+            try
+            {
+                config = File.ReadAllText("config\\pc2pos.json");
+                var configPort = JsonConvert.DeserializeObject<ConfigPort>(config);
+                int i = cmb3in1.FindString("COM" + configPort.port.ToString());
+                if (i >= 0)
+                {
+                    cmb3in1.SelectedIndex = i;
+                }
+                else
+                {
+                    int iNew = cmb3in1.Items.Add("COM" + configPort.port.ToString());
+                    cmb3in1.SelectedIndex = iNew;
+                }
+                cmb3in1.Update();
+            }
+            catch (FileNotFoundException) { }
+
+        }
+
+        private void lbl3in1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
